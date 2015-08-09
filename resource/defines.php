b49a26c8e46575b09b85bf4e8e63b1a59cf17588
one-more <greyjoi@yandex.ru>
2015-08-09 19:32:29 +0300

    add WEB_ROOT constant

--- resource/defines.php
+++ resource/defines.php
@@ -6,6 +6,9 @@ if(!defined('ROOT_PATH')) {
 if(!defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
+if(!defined('WEB_ROOT')) {
+    define('WEB_ROOT', ROOT_PATH.DS.'www');
+}
 if(!defined('SMARTY_DIR')) {
 	define('SMARTY_DIR', ROOT_PATH.DS.'lib'.DS.'Smarty'.DS);
 }
